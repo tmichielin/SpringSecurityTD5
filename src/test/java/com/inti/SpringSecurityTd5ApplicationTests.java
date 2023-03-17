150670a568469432955b388b8a4ffe8490c00a0d
theod <theod@DESKTOP-VBIVKTD>
2023-03-17 09:48:45 +0100

    Mise en place

---
+++ src/test/java/com/inti/SpringSecurityTd5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityTd5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
